96f0aa96de1b6fdf0e6aaa452cd8dc41fadfc49f
Andy Winslow <awinslow@bluetarp.com>
2015-07-07 16:13:03 -0400

    attempting to use special base url variable to point to Jira host

--- jira_gadgets/versions_to_be_released/versions_to_be_released.js
+++ jira_gadgets/versions_to_be_released/versions_to_be_released.js
@@ -13,9 +13,8 @@ var numEntries = prefs.getInt("num_entries");
 gadgets.util.registerOnLoadHandler(fetchIssues);
 
 function fetchIssues() {
-  // Request URL for most recently created,
-  // unresolved issues on jira.atlassian.com
-  var url = '/rest/api/2/search?maxResults=1000&fields=fixVersions&jql=category%20%3D%20"Software%20Development"%20AND%20issuetype%20!%3D%20Sub-task%20AND%20fixVersion%20is%20not%20EMPTY%20AND%20fixVersion%20not%20in%20releasedVersions()%20AND%20status%20in%20(Developing%2C%20"Development%20Complete"%2C%20Testing%2C%20Accepting%2C%20"Waiting%20to%20Deploy")%20ORDER%20BY%20project%2C%20fixVersion';
+  // Using Jira REST API, search for unreleased issues with a fixVersion
+  var url = '__ATLASSIAN_BASE_URL__/rest/api/2/search?maxResults=1000&fields=fixVersions&jql=category%20%3D%20"Software%20Development"%20AND%20issuetype%20!%3D%20Sub-task%20AND%20fixVersion%20is%20not%20EMPTY%20AND%20fixVersion%20not%20in%20releasedVersions()%20AND%20status%20in%20(Developing%2C%20"Development%20Complete"%2C%20Testing%2C%20Accepting%2C%20"Waiting%20to%20Deploy")%20ORDER%20BY%20project%2C%20fixVersion';
 
   // Construct request parameters object
   var params = {};
